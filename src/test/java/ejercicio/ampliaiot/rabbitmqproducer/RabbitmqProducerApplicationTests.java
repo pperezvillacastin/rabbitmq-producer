e1f49cde32f6ee0badacbec7790adf4bb405d3d2
Pablo <pablo.perez@techedgegroup.com>
2022-07-26 12:13:40 +0200

    Inicializado proyecto con Spring Initializr

---
+++ src/test/java/ejercicio/ampliaiot/rabbitmqproducer/RabbitmqProducerApplicationTests.java
@@ -0,0 +1,13 @@
+package ejercicio.ampliaiot.rabbitmqproducer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RabbitmqProducerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
