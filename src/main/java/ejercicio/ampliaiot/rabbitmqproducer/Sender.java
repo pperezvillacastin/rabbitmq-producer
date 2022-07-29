9693a4f1209881e7a5885e80ee133f41d9fdf7a6
Pablo <pablo.perez@techedgegroup.com>
2022-07-29 21:52:02 +0200

    Cargando recurso

--- src/main/java/ejercicio/ampliaiot/rabbitmqproducer/Sender.java
+++ src/main/java/ejercicio/ampliaiot/rabbitmqproducer/Sender.java
@@ -6,12 +6,12 @@ import org.apache.commons.lang3.exception.ExceptionUtils;
 import org.springframework.amqp.core.Queue;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.io.Resource;
 import org.springframework.scheduling.annotation.Scheduled;
-import org.springframework.util.ResourceUtils;
-
+import org.springframework.util.FileCopyUtils;
 import java.io.*;
-import java.nio.file.Files;
-import java.nio.file.Path;
+import java.nio.charset.StandardCharsets;
 
 @Log
 public class Sender {
@@ -21,12 +21,14 @@ public class Sender {
     @Autowired
     private Queue queue;
 
+    @Value("classpath:formatoOpenGateTemplate.json")
+    private Resource templateResource;
+
     @Scheduled(fixedDelay = 2000, initialDelay = 500)
     public void send(){
         log.info("Preparing to send");
         try {
-            Path templateFilePath = ResourceUtils.getFile("classpath:formatoOpenGateTemplate.json").toPath();
-            String templateString = Files.readString(templateFilePath);
+            String templateString = FileCopyUtils.copyToString(new InputStreamReader(templateResource.getInputStream(), StandardCharsets.UTF_8));
             Double randomDouble = RandomUtils.nextDouble(0.0, 50);
             String finalMessage = templateString.replace("REPLACEME", randomDouble.toString());
             this.template.convertAndSend(queue.getName(), finalMessage);
